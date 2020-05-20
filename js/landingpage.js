77f18e755f4ca507804e95340af8d33ba41a4ca3
Cameron <cjpuffnstuff@gmail.com>
2020-05-19 23:08:23 -0700

    Initial Commit

---
+++ js/landingpage.js
@@ -0,0 +1,40 @@
+let searchEngine = 1; /* 0= google 1= duckduckgo 2= youtube */
+$(document).ready(function () {
+  $("#searchBox").focus(); /* Focuses the search bar on load so it can be typed into imediatly */
+});
+
+document.onkeydown = function(e) {
+
+  if (e.which == 13) {
+
+    if (searchEngine == 1) {
+      window.location.href = "https://duckduckgo.com/?q=" + $("#searchBox").val();
+    } else if (searchEngine == 0) {
+      window.location.href = "https://google.com/search?q=" + $("#searchBox").val();
+    } else {
+      window.location.href = "https://youtube.com/search?q=" + $("#searchBox").val();
+    }
+
+  } else {
+
+    if (e.which == 37) {
+      if (searchEngine > 0) {
+        searchEngine--;
+      }
+    } else if (e.which == 39) {
+      if (searchEngine < 2) {
+        searchEngine++;
+      }
+    }
+    $("h2").removeClass("selected");
+    if (searchEngine == 0) {
+      $("#google-engine").addClass("selected");
+    } else if (searchEngine == 1) {
+      $("#duckduckgo-engine").addClass("selected");
+    } else {
+      $("#youtube-engine").addClass("selected");
+    }
+
+  }
+
+};
